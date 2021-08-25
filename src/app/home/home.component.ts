bbee7639aa479ff1002e54018f12edb220b9832a
karelinho <karel.sluka@gmail.com>
2021-08-25 12:33:24 +0200

    create user interface.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,9 +9,75 @@ import { DataService, DataItem } from '../shared/data.service'
 export class HomeComponent implements OnInit {
   items: Array<DataItem>
 
+  value1 = 1;
+
+  value2 = 1;
+
+  value3 = 1;
+
+  value4 = 1;
+
+  value5 = 1;
+
+  value6 = 1;
+
+  value7 = 1;
+
+  value8 = 1;
+
+  value9 = 1;
+
+  value10 = 1;
+
+  value11 = 1;
+
   constructor(private _itemService: DataService) {}
 
   ngOnInit(): void {
     this.items = this._itemService.getItems()
+  }
+
+  onValue1Change(value: number) {
+    this.value1 = value;
+  }
+
+  onValue2Change(value: number) {
+    this.value2 = value;
+  }
+
+  onValue3Change(value: number) {
+    this.value3 = value;
+  }
+
+  onValue4Change(value: number) {
+    this.value4 = value;
+  }
+
+  onValue5Change(value: number) {
+    this.value5 = value;
+  }
+
+  onValue6Change(value: number) {
+    this.value6 = value;
+  }
+
+  onValue7Change(value: number) {
+    this.value7 = value;
+  }
+
+  onValue8Change(value: number) {
+    this.value8 = value;
+  }
+
+  onValue9Change(value: number) {
+    this.value9 = value;
+  }
+
+  onValue10Change(value: number) {
+    this.value10 = value;
+  }
+
+  onValue11Change(value: number) {
+    this.value11 = value;
   }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,9 +4,14 @@ import { NativeScriptCommonModule } from '@nativescript/angular'
 import { HomeRoutingModule } from './home-routing.module'
 import { HomeComponent } from './home.component'
 import { ItemDetailComponent } from './item-detail/item-detail.component'
+import { TNSCheckBoxModule } from '@nstudio/nativescript-checkbox/angular';
 
 @NgModule({
-  imports: [NativeScriptCommonModule, HomeRoutingModule],
+  imports: [
+    NativeScriptCommonModule,
+    HomeRoutingModule,
+    TNSCheckBoxModule
+  ],
   declarations: [HomeComponent, ItemDetailComponent],
   schemas: [NO_ERRORS_SCHEMA],
 })
